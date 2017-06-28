23fa8c4935c8c9576b0553ef2d84b705ded3c43b
Eric Swanson <eswanson@alloscomp.com>
2017-06-28 10:12:32 -0700

    Fix Comcast to work with AJAX or HTML

--- comcast.py
+++ comcast.py
@@ -43,71 +43,78 @@ logger.debug("Posting to login...")
 res = session.post('https://login.comcast.net/login', data=data)
 assert res.status_code == 200
 
-logger.debug("Preloader HTML...")
-res = session.get('https://customer.xfinity.com/Secure/Preloading/?backTo=%2fMyServices%2fInternet%2fUsageMeter%2f')
-assert res.status_code == 200
+try:
+    logger.debug("Fetching internet usage AJAX...")
+    res = session.get('https://customer.xfinity.com/apis/services/internet/usage')
+    assert res.status_code == 200
 
-logger.debug("Preloader AJAX...")
-res = session.get('https://customer.xfinity.com/Secure/Preloader.aspx')
-assert res.status_code == 200
+    print(res.text)
+except Exception:
+    logger.debug("Preloader HTML...")
+    res = session.get('https://customer.xfinity.com/Secure/Preloading/?backTo=%2fMyServices%2fInternet%2fUsageMeter%2f')
+    assert res.status_code == 200
 
-logger.debug("Waiting 5 seconds for preloading to complete...")
-time.sleep(5)
+    logger.debug("Preloader AJAX...")
+    res = session.get('https://customer.xfinity.com/Secure/Preloader.aspx')
+    assert res.status_code == 200
 
-logger.debug("Fetching internet usage HTML...")
-res = session.get('https://customer.xfinity.com/MyServices/Internet/UsageMeter/')
-assert res.status_code == 200
-html = res.text
+    logger.debug("Waiting 5 seconds for preloading to complete...")
+    time.sleep(5)
 
-# Example HTML:
-#    <div class="cui-panel-body">
-#        <!-- data-options: 
-#                unit (string) - fills in labels; example: GB, MB, miles; 
-#                max (number, optional) - the 100% number of the bar
-#                increment (number, optional) - the number between grid lines 
-#                -->
-#        <span class="cui-usage-label"><p>Home Internet Usage</p></span>
-#        <div data-component="usage-meter"
-#            data-options="hideMax:true;divisions:4;
-#                          unit:GB;
-#                          max:1024;
-#                          increment:50
-#                          ">
-#            <div class="cui-usage-bar" data-plan="1024">
-#                <span data-used="222" data-courtesy="false">
-#                    <span class="accessibly-hidden">222GB of 1024GB</span>
-#                </span>
-#            </div>
-#            <div class="cui-usage-label">
-#                <span>
-#                    222GB of 1024GB
-#                </span>
-#                <!--<p><a href="#">View details</a></p>-->
-#                <span class="marker"></span>
-#            </div>
-#        </div>
+    logger.debug("Fetching internet usage HTML...")
+    res = session.get('https://customer.xfinity.com/MyServices/Internet/UsageMeter/')
+    assert res.status_code == 200
+    html = res.text
 
-used = None
-m = re.search(r'<span data-used="(\d+)"', html)
-if m:
-    used = int(m.group(1))
+    # Example HTML:
+    #    <div class="cui-panel-body">
+    #        <!-- data-options: 
+    #                unit (string) - fills in labels; example: GB, MB, miles; 
+    #                max (number, optional) - the 100% number of the bar
+    #                increment (number, optional) - the number between grid lines 
+    #                -->
+    #        <span class="cui-usage-label"><p>Home Internet Usage</p></span>
+    #        <div data-component="usage-meter"
+    #            data-options="hideMax:true;divisions:4;
+    #                          unit:GB;
+    #                          max:1024;
+    #                          increment:50
+    #                          ">
+    #            <div class="cui-usage-bar" data-plan="1024">
+    #                <span data-used="222" data-courtesy="false">
+    #                    <span class="accessibly-hidden">222GB of 1024GB</span>
+    #                </span>
+    #            </div>
+    #            <div class="cui-usage-label">
+    #                <span>
+    #                    222GB of 1024GB
+    #                </span>
+    #                <!--<p><a href="#">View details</a></p>-->
+    #                <span class="marker"></span>
+    #            </div>
+    #        </div>
 
-total = None
-m = re.search(r'<div class="cui-usage-bar" data-plan="(\d+)">', html)
-if m:
-    total = int(m.group(1))
-
-unit = None
-m = re.search(r'<div data-component="usage-meter"\s*data-options="([^"]*)"', html)
-if m:
-    opts = m.group(1)
-    opts = re.sub(r'\s+', '', opts)  # remove whitespace
-    m = re.search(r'unit:(\w+);', opts)
+    used = None
+    m = re.search(r'<span data-used="(\d+)"', html)
     if m:
-        unit = m.group(1)
+        used = int(m.group(1))
 
-print(json.dumps({
-    'used': used,
-    'total': total,
-    'unit': unit,
-}))
+    total = None
+    m = re.search(r'<div class="cui-usage-bar" data-plan="(\d+)">', html)
+    if m:
+        total = int(m.group(1))
+
+    unit = None
+    m = re.search(r'<div data-component="usage-meter"\s*data-options="([^"]*)"', html)
+    if m:
+        opts = m.group(1)
+        opts = re.sub(r'\s+', '', opts)  # remove whitespace
+        m = re.search(r'unit:(\w+);', opts)
+        if m:
+            unit = m.group(1)
+
+    print(json.dumps({
+        'used': used,
+        'total': total,
+        'unit': unit,
+    }))
